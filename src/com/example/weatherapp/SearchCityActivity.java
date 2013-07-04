382dda207a89080d57410c76a8c083f3fd513e2e
katsiaryna <katsiaryna.pomazai@tut.by>
2013-07-05 02:08:24 +0300

    оооо

--- src/com/example/weatherapp/SearchCityActivity.java
+++ src/com/example/weatherapp/SearchCityActivity.java
@@ -97,7 +97,7 @@ public class SearchCityActivity extends Activity implements OnItemLongClickListe
 			}
 		});
 		
-		// создаем диалог jjj
+		// создаем диалог jjjjjj
 		return alertDialogBuilder.create();
 		}
 		return super.onCreateDialog(id);
